1fdbf5ca9e785d544da2893bf7c1ef2a69504fd2
Aldara Fernandez Garcia <aldferna@c2r12s6.42madrid.com>
2025-02-03 20:03:38 +0100

    e

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aldferna <aldferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 13:49:00 by aldferna          #+#    #+#             */
-/*   Updated: 2025/02/03 18:39:57 by aldferna         ###   ########.fr       */
+/*   Updated: 2025/02/03 20:01:10 by aldferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,11 +52,36 @@ char	**search_path(char **env, char *comnd)
 	return (paths);
 }
 
-void	first_child(int *connect, char **argv, char **env)
+void	exe(char **env, char **comnd)
 {
-	int		fd_in;
 	char	**paths;
 	int		i;
+
+	if ((ft_strchr(comnd[0], '/') != NULL) && (access(comnd[0], X_OK) == 0))
+		execve(comnd[0], comnd, env);
+	else
+	{
+		paths = search_path(env, comnd[0]);
+		i = 0;
+		while (paths[i] != NULL)
+		{
+			if (access(paths[i], R_OK | X_OK) == 0)
+				execve(paths[i], comnd, env);
+			i++;
+		}
+		i = 0;
+		while (paths[i])
+		{
+			free(paths[i]);
+			i++;
+		}
+		free(paths);
+	}
+}
+
+void	first_command(int *connect, char **argv, char **env)
+{
+	int		fd_in;
 	char	**comnd1;
 
 	close(connect[0]);
@@ -73,28 +98,14 @@ void	first_child(int *connect, char **argv, char **env)
 	close(fd_in);
 	dup2(connect[1], 1);
 	close(connect[1]);
-	if ((ft_strchr(comnd1[0], '/') != NULL) && (access(comnd1[0], X_OK) == 0))
-		execve(comnd1[0], comnd1, env);
-	else
-	{
-		paths = search_path(env, comnd1[0]);
-		i = 0;
-		while (paths[i] != NULL)
-		{
-			if (access(paths[i], R_OK | X_OK) == 0)
-				execve(paths[i], comnd1, env);
-			i++;
-		}
-	}
+	exe(env, comnd1);
 	perror("error command 1");
 	exit(5);
 }
 
-void	second_child(int *connect, char **argv, char **env)
+void	second_command(int *connect, char **argv, char **env)
 {
 	int		fd_out;
-	char	**paths;
-	int		i;
 	char	**comnd2;
 
 	close(connect[1]);
@@ -111,21 +122,17 @@ void	second_child(int *connect, char **argv, char **env)
 	close(connect[0]);
 	dup2(fd_out, 1);
 	close(fd_out);
-	if ((ft_strchr(comnd2[0], '/') != NULL) && (access(comnd2[0], X_OK) == 0))
-		execve(comnd2[0], comnd2, env);
-	else
-	{
-		paths = search_path(env, comnd2[0]);
-		i = 0;
-		while (paths[i] != NULL)
-		{
-			if (access(paths[i], R_OK | X_OK) == 0)
-				execve(paths[i], comnd2, env);
-			i++;
-		}
-	}
+	exe(env, comnd2);
 	perror("error command 2");
 	exit(9);
+}
+
+void end(int *connect, int pid_1, int pid_2)
+{
+    close(connect[1]);
+    close(connect[0]);
+    waitpid(pid_1, NULL, 0);
+    waitpid(pid_2, NULL, 0);
 }
 
 int	main(int argc, char **argv, char **env)
@@ -134,32 +141,25 @@ int	main(int argc, char **argv, char **env)
 	pid_t	pid_2;
 	int		connect[2];
 
-	if (argc != 5 || argv[1][0] == '\0' || argv[2][0] == '\0'
-		|| argv[3][0] == '\0' || argv[4][0] == '\0')
+	if (argc != 5 || argv[2][0] == '\0' || argv[3][0] == '\0')
 	{
-		write(1, "empty param\n", 12);
+		write(1, "missing param\n", 14);
 		exit(1);
 	}
-	pipe(connect);
+	pipe(connect); //init /medium / end y cada abre su pipe y hace su fork devuelven lo que necesita el siguiente hijo
 	pid_1 = fork();
 	if (pid_1 == -1)
 		exit(2);
 	if (pid_1 == 0)
-		first_child(connect, argv, env);
+		first_command(connect, argv, env);
 	else
 	{
 		pid_2 = fork();
 		if (pid_2 == -1)
 			exit(6);
 		if (pid_2 == 0)
-			second_child(connect, argv, env);
+			second_command(connect, argv, env);
 		else
-		{
-			close(connect[1]);
-			close(connect[0]);
-			waitpid(pid_1, NULL, 0);
-			waitpid(pid_2, NULL, 0);
-			return (0);
-		}
+            end(connect, pid_1, pid_2);
 	}
 }
