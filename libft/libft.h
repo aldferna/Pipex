d24ccbf8847478affdc91e983ecc9461d3ea7363
Aldara Fernandez Garcia <aldferna@c2r12s6.42madrid.com>
2025-02-03 19:39:28 +0100

    3feb

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: aldferna <aldferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:57:43 by aldferna          #+#    #+#             */
-/*   Updated: 2025/02/01 19:35:50 by aldferna         ###   ########.fr       */
+/*   Updated: 2025/02/03 17:08:31 by aldferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strjoin(const char *s1, const char *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
 #endif
--- main.c
+++ main.c
@@ -6,122 +6,160 @@
 /*   By: aldferna <aldferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 13:49:00 by aldferna          #+#    #+#             */
-/*   Updated: 2025/02/01 20:19:43 by aldferna         ###   ########.fr       */
+/*   Updated: 2025/02/03 18:39:57 by aldferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft/libft.h"
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include "libft/libft.h"
+#include <unistd.h>
 
-char **search_path(char **env, char *comnd)
+char	**search_path(char **env, char *comnd)
 {
-    char **paths;
-    char *aux;
-    char *new_path;
-    int i;
+	char	**paths;
+	char	*aux;
+	char	*new_path;
+	int		i;
 
-    i = 0;
-    while (env[i] != NULL)
-    {
-        if (ft_strncmp(env[i], "PATH=", 5) == 0)
-            break;
-        i++;
-    }
-    paths = ft_split(env[i] + 5, ':'); //++ en el split; me muevo sin modificar
-    i = 0;
-    while (paths[i] != NULL)
-    {
-        aux = ft_strjoin(paths[i], "/");
-        new_path = ft_strjoin(aux, comnd);
-        free(paths[i]);
-        paths[i] = new_path;
-        free(aux);
-        i++;
-    }
-    return (paths);
+	i = 0;
+	while (env[i] != NULL)
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+			break ;
+		i++;
+	}
+	if (env[i] == NULL)
+	{
+		write(2, "Path not found\n", 16);
+		exit(10);
+	}
+	paths = ft_split(env[i] + 5, ':');
+	i = 0;
+	while (paths[i] != NULL)
+	{
+		aux = ft_strjoin(paths[i], "/");
+		new_path = ft_strjoin(aux, comnd);
+		free(paths[i]);
+		paths[i] = new_path;
+		free(aux);
+		i++;
+	}
+	return (paths);
 }
 
-int main (int argc, char **argv, char **env)
+void	first_child(int *connect, char **argv, char **env)
 {
-    pid_t pid_1;
-    pid_t pid_2;
-    int fd_in;
-    int fd_out;
-    char **comnd1;
-    char **comnd2;
-    int connect[2];
-    char **paths;
-    int i;
+	int		fd_in;
+	char	**paths;
+	int		i;
+	char	**comnd1;
 
-    if (argc != 5)
-        exit (1);
-    pipe(connect);
-    pid_1 = fork();
-    if (pid_1 == -1)
-        exit (2);
-    if (pid_1 == 0)
-    {
-        close(connect[0]);
-        fd_in = open (argv[1], O_RDONLY);
-        if (fd_in == -1)
-            exit (3);
-        comnd1 = ft_split(argv[2], ' ');
-        if (!comnd1)
-            exit (4);
-        dup2(fd_in, 0);
-        close(fd_in);
-        dup2(connect[1], 1);
-        close(connect[1]);
-        paths = search_path(env, comnd1[0]);
-        i = 0;
-        while (paths[i] != NULL)
-        {
-            if (access(paths[i], X_OK) == 0)
-                execve(paths[i], comnd1, env);
-            i++;
-        }
-        exit (5);
-    }
-    else
-    {
-        pid_2 = fork();
-        if (pid_2 == -1)
-            exit (6);
-        if (pid_2 == 0)
-        {
-            close(connect[1]);
-            fd_out = open(argv[4], O_CREAT|O_RDWR|O_TRUNC, 0644);
-            if (fd_out == -1)
-                exit (7);
-            comnd2 = ft_split(argv[3], ' ');
-            if (!comnd2)
-                exit (8);
-            dup2(connect[0], 0);
-            close(connect[0]);
-            dup2(fd_out, 1);
-            close(fd_out);
-            paths = search_path(env, comnd2[0]);
-            i = 0;
-            while (paths[i] != NULL)
-            {
-                if (access(paths[i], X_OK) == 0)
-                    execve(paths[i], comnd2, env);
-                i++;
-            }
-            exit (9);
-        }
-        else //aqui algo mas?
-        {
-            close (connect[1]);
-            close (connect[0]);
-            waitpid(pid_1, NULL, 0);
-            waitpid(pid_2, NULL, 0);
-            return (0);
-        }
-    }
+	close(connect[0]);
+	fd_in = open(argv[1], O_RDONLY);
+	if (fd_in < 0)
+	{
+		perror("error input file");
+		exit(3);
+	}
+	comnd1 = ft_split(argv[2], ' ');
+	if (!comnd1)
+		exit(4);
+	dup2(fd_in, 0);
+	close(fd_in);
+	dup2(connect[1], 1);
+	close(connect[1]);
+	if ((ft_strchr(comnd1[0], '/') != NULL) && (access(comnd1[0], X_OK) == 0))
+		execve(comnd1[0], comnd1, env);
+	else
+	{
+		paths = search_path(env, comnd1[0]);
+		i = 0;
+		while (paths[i] != NULL)
+		{
+			if (access(paths[i], R_OK | X_OK) == 0)
+				execve(paths[i], comnd1, env);
+			i++;
+		}
+	}
+	perror("error command 1");
+	exit(5);
+}
+
+void	second_child(int *connect, char **argv, char **env)
+{
+	int		fd_out;
+	char	**paths;
+	int		i;
+	char	**comnd2;
+
+	close(connect[1]);
+	fd_out = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (fd_out < 0)
+	{
+		perror("error output file");
+		exit(7);
+	}
+	comnd2 = ft_split(argv[3], ' ');
+	if (!comnd2)
+		exit(8);
+	dup2(connect[0], 0);
+	close(connect[0]);
+	dup2(fd_out, 1);
+	close(fd_out);
+	if ((ft_strchr(comnd2[0], '/') != NULL) && (access(comnd2[0], X_OK) == 0))
+		execve(comnd2[0], comnd2, env);
+	else
+	{
+		paths = search_path(env, comnd2[0]);
+		i = 0;
+		while (paths[i] != NULL)
+		{
+			if (access(paths[i], R_OK | X_OK) == 0)
+				execve(paths[i], comnd2, env);
+			i++;
+		}
+	}
+	perror("error command 2");
+	exit(9);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	pid_t	pid_1;
+	pid_t	pid_2;
+	int		connect[2];
+
+	if (argc != 5 || argv[1][0] == '\0' || argv[2][0] == '\0'
+		|| argv[3][0] == '\0' || argv[4][0] == '\0')
+	{
+		write(1, "empty param\n", 12);
+		exit(1);
+	}
+	pipe(connect);
+	pid_1 = fork();
+	if (pid_1 == -1)
+		exit(2);
+	if (pid_1 == 0)
+		first_child(connect, argv, env);
+	else
+	{
+		pid_2 = fork();
+		if (pid_2 == -1)
+			exit(6);
+		if (pid_2 == 0)
+			second_child(connect, argv, env);
+		else
+		{
+			close(connect[1]);
+			close(connect[0]);
+			waitpid(pid_1, NULL, 0);
+			waitpid(pid_2, NULL, 0);
+			return (0);
+		}
+	}
 }
